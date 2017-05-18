d3de1c7036a6a5d7bf775bc5b744c8b86db9ecfd
regireed89 <regireed89@gmail.com>
2017-05-18 17:30:41 -0500

    display button

--- Assets/Scripts/LootTable.cs
+++ Assets/Scripts/LootTable.cs
@@ -1,15 +1,17 @@
-﻿using System.Collections; 
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Security.Policy;
 using ScriptableObjects;
-using UnityEngine; 
+using UnityEngine;
+using UnityEditor;
 
 [CreateAssetMenu(fileName = "Loot Table", menuName = "Loot Table", order = 1)]
 public class LootTable : ScriptableObject
 {
 
-    public List<ItemDrop> Drops;
 
+    public List<ItemDrop> Drops;
+    public float r;
     [System.Serializable]
     public class ItemDrop
     {
@@ -20,20 +22,38 @@ public class LootTable : ScriptableObject
 
     }
 
+
+
     public List<Item> GetDrops()
     {
         List<Item> items = new List<Item>();
-        float r = Random.Range(0.0f, 1.0f);
+        r = Random.Range(0.0f, 1.0f);
         foreach (ItemDrop drop in Drops)
         {
             if (drop.ChanceToDrop > r)
             {
-                
                 items.Add(drop.Item);
             }
         }
         return items;
     }
+    [CustomEditor(typeof(LootTable))]
+    public class InspectorLootTable:Editor
+    {
+        
+        public override void OnInspectorGUI()
+        {
+            
+            var mytarget = target as LootTable;
+            if (GUILayout.Button("Roll"))
+            {
+                mytarget.GetDrops();
+            }
+            base.OnInspectorGUI();
+        }
+}
+    
+
 
 }
  
